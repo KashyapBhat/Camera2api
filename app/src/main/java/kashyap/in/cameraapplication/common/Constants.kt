2022166759e262b1475eb6af2a072a781c96ecef
Kashyap <bobbikashyap@gmail.com>
2020-07-31 22:58:19 +0530

    --- camera 2 api fragment to activity conversion

--- app/src/main/java/kashyap/in/cameraapplication/common/Constants.kt
+++ app/src/main/java/kashyap/in/cameraapplication/common/Constants.kt
@@ -2,3 +2,7 @@ package kashyap.`in`.cameraapplication.common
 
 const val FRONT_CAM: String = "front"
 const val BACK_CAM: String = "back"
+
+const val CAMERA_ID = "camera_id"
+const val CAMERA_FRONT = "1"
+const val CAMERA_BACK = "0"
